37c64615efc56f804b1664cbb5f8fb56a6f921c5
john-ashraf-7 <johnashraf@aucegypt.edu>
2023-10-15 21:15:50 +0300

    first version

---
+++ test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+void DisplayPalindromes(string input)
+{
+  int size = input.size();
+  int pivot = 0;
+  for(int i=0;i<size;i++)
+  {
+    bool correct = true;
+    int incrementer = 0;
+    int decrementer = 0;
+    while(correct)
+    {
+      if(input[pivot+decrementer] == input[pivot+incrementer])
+      {
+        
+      }
+      else
+      {
+
+      }
+    }
+    pivot++;
+    incrementer++;
+    decrementer--;
+
+  }
+
+}
+
+int main()
+{
+  string input;
+  cin >> input;
+  DisplayPalindromes(input);
+
+}
