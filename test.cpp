cf563e3e563e42982e774524f6a39fc30e12c2a8
john-ashraf-7 <johnashraf@aucegypt.edu>
2023-10-15 21:22:00 +0300

    h

--- test.cpp
+++ test.cpp
@@ -1,42 +1,7 @@
 #include <iostream>
 using namespace std;
 
-
-void DisplayPalindromes(string input)
+main()
 {
-  int size = input.size();
-  int pivot = 0;
-  for(int i=0;i<size;i++)
-  {
-    bool correct = true;
-    int incrementer = 0;
-    int decrementer = 0;
-    cout << "test" << endl;
-    while(correct)
-    {
-      if(input[pivot+decrementer] == input[pivot+incrementer])
-      {
-        //in here, we need to complete the code.
-        cout << "hi guys" << endl;
-        cout << "wassup ayy" << endl;
-      }
-      else
-      {
-
-      }
-    }
-    pivot++;
-    incrementer++;
-    decrementer--;
-
-  }
-
-}
-
-int main()
-{
-  string input;
-  cin >> input;
-  DisplayPalindromes(input);
-
+  cout << "hi" << endl;
 }
